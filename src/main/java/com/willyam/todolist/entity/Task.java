d94dbbdc748ef330b44369f7f401ceeb4d31f7f0
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-08-21 16:15:45 -0300

    feat: adding task

---
+++ src/main/java/com/willyam/todolist/entity/Task.java
@@ -0,0 +1,20 @@
+package com.willyam.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+}
