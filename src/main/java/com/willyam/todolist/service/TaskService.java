04746148f19d8725c92d581d172d5684e2787428
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-08-21 16:16:05 -0300

    feat: adding task service

---
+++ src/main/java/com/willyam/todolist/service/TaskService.java
@@ -0,0 +1,17 @@
+package com.willyam.todolist.service;
+
+import com.willyam.todolist.entity.Task;
+import com.willyam.todolist.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class TaskService {
+
+    public Task getAllTasks() {
+        return [];
+    }
+
+}
