ea1bb2056c0c53b73efdb80a5dfa15fe8543b8a2
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-08-22 15:02:34 -0300

    refactor: adding task repo

--- src/main/java/com/willyam/todolist/repository/TaskRepository.java
+++ src/main/java/com/willyam/todolist/repository/TaskRepository.java
@@ -1,12 +1,10 @@
 package com.willyam.todolist.repository;
 
-import  com.willyam.todolist.entity.Task;
+import com.willyam.todolist.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TaskRepository<Task, Long> {
-    public interface TaskRepository extends JpaRepository<Task, Long>{
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    }
 }
