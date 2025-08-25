57ac84266498391ee296c3028bdfc579eae8279e
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-08-25 14:55:48 -0300

    refactor: importing java list

--- src/main/java/com/willyam/todolist/repository/TaskRepository.java
+++ src/main/java/com/willyam/todolist/repository/TaskRepository.java
@@ -2,11 +2,10 @@ package com.willyam.todolist.repository;
 
 import com.willyam.todolist.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
     List<Task> findByCompletedTrue();
     List<Task> findByCompletedFalse();
 
